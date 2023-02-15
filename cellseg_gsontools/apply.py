f7c39c68090b3a48f25410d6ced15e2941c6080b
Okunator <oskari.lehtonen@helsinki.fi>
2023-02-15 17:54:32 +0200

    refactor(apply): add column opt for apply

--- cellseg_gsontools/apply.py
+++ cellseg_gsontools/apply.py
@@ -9,6 +9,7 @@ __all__ = ["gdf_apply"]
 def gdf_apply(
     gdf: gpd.GeoDataFrame,
     func: Callable,
+    col: str = "geometry",
     parallel: bool = False,
     pbar: bool = False,
     **kwargs,
@@ -21,22 +22,32 @@ def gdf_apply(
             Input GeoDataFrame.
         func : Callable
             A callable function.
+        col : str, default="geometry"
+            The name of the column of the gdf that is used as the input
+            to apply operation.
         parallel : bool, default=False
             Flag, whether to parallelize the operation with pandarallel
         pbar : bool, default=False
             Show progress bar when executing in parallel mode. Ignored if
             `parallel=False`
+        **kwargs:
+            Arbitrary keyword args for the `func` callable.
 
     Returns
     -------
         gpd.GeoSeries
             A GeoSeries object containing the metrics computed for each polygon in the
             input gdf.
+
+    Examples
+    --------
+        >>> # Get the compactness of the polygons in a gdf
+        >>> gdf["compactness"] = gdf_apply(gdf, compactness, parallel=True)
     """
     if not parallel:
-        res = gdf.geometry.apply(func, **kwargs)
+        res = gdf[col].apply(func, **kwargs)
     else:
         pandarallel.initialize(verbose=1, progress_bar=pbar)
-        res = gdf.geometry.parallel_apply(func, **kwargs)
+        res = gdf[col].parallel_apply(func, **kwargs)
 
     return res
