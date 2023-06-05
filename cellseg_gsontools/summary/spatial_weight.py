e2c29cf364f9bdab98bab794ed404434e2a34607
Okunator <oskari.lehtonen@helsinki.fi>
2023-06-05 13:57:25 +0300

    refactor: unify summary obj I/O in spatial w sum

--- cellseg_gsontools/summary/spatial_weight.py
+++ cellseg_gsontools/summary/spatial_weight.py
@@ -5,7 +5,6 @@ import pandas as pd
 from libpysal.weights import W
 
 from ..links import link_counts
-from ..spatial_context._base import _SpatialContext
 from ..summary._base import Summary
 
 __all__ = ["SpatialWeightSummary"]
@@ -14,16 +13,19 @@ __all__ = ["SpatialWeightSummary"]
 class SpatialWeightSummary(Summary):
     def __init__(
         self,
-        spatial_context: _SpatialContext,
-        classes: Optional[Tuple[str, ...]] = None,
+        spatial_weights: W,
+        gdf: gpd.GeoDataFrame,
+        classes: Tuple[str, ...],
         prefix: str = None,
     ) -> None:
         """Create a summary object for the cell networks in a _SpatialContext obj.
 
         Parameters
         ----------
-            spatial_context : _SpatialContext
-                A spatial context object. E.g `InterfaceContext`, or `WithinContext`.
+            spatial_weights : W
+                A libpysal spatial weights object.
+            gdf : gpd.GeoDataFrame
+                The input geodataframe.
             classes : Tuple[str, ...], optional
                 The cell type classes found in the data. Optional.
             prefix : str, optional
@@ -38,62 +40,59 @@ class SpatialWeightSummary(Summary):
             link_props : pd.DataFrame
                 A dataframe containing link-proportions for each context area
 
-        Raises
-        ------
-            ValueError if input is not a _SpatiaContext object
-
         Example
         -------
-            Compute the link-counts in the tumor-stroma interface of a slide.
+        Compute the link-counts in the tumor-stroma interface of a slide.
 
-            >>> from cellseg_gsontools.spatial_context import InterfaceContext
-            >>> iface_context = InterfaceContext(
-                    area_gdf=areas,
-                    cell_gdf=cells,
-                    label1="area_cin",
-                    label2="areastroma",
-                    silence_warnings=True,
-                    verbose=True,
-                    min_area_size=100000.0
-                )
+        >>> from cellseg_gsontools.spatial_context import InterfaceContext
+        >>> iface_context = InterfaceContext(
+        ...     area_gdf=areas,
+        ...     cell_gdf=cells,
+        ...     label1="area_cin",
+        ...     label2="areastroma",
+        ...     silence_warnings=True,
+        ...     verbose=True,
+        ...     min_area_size=100000.0
+        ... )
+        >>> iface_context.fit(verbose=False)
 
-            >>> t = [
-                    "inflammatory",
-                    "connective",
-                    "glandular_epithel",
-                    "squamous_epithel",
-                    "neoplastic"
-                ]
+        >>> classes = [
+        ...     "inflammatory",
+        ...     "connective",
+        ...     "glandular_epithel",
+        ...     "squamous_epithel",
+        ...     "neoplastic",
+        ... ]
 
-            >>> s = SpatialWeightSummary(iface_context, t, prefix="n-")
-            >>> s.summarize("border_network")
-            Processing interface area: 4: 100%|██████████| 4/4 [00:01<00:00,  2.21it/s]
-            n-inflammatory-inflammatory               31
-            n-inflammatory-connective                 89
-            n-inflammatory-glandular_epithel           0
-            n-inflammatory-squamous_epithel            0
-            n-inflammatory-neoplastic                 86
-            n-connective-connective                  131
-            n-connective-glandular_epithel             0
-            n-connective-squamous_epithel              0
-            n-connective-neoplastic                  284
-            n-glandular_epithel-glandular_epithel      0
-            n-glandular_epithel-squamous_epithel       0
-            n-glandular_epithel-neoplastic             0
-            n-squamous_epithel-squamous_epithel        0
-            n-squamous_epithel-neoplastic              0
-            n-neoplastic-neoplastic                  236
-            dtype: int64
+        >>> ss = SpatialWeightSummary(
+        ...     iface_context.merge_weights("border_network"),
+        ...     iface_context.cell_gdf,
+        ...     classes=classes,
+        ...     prefix="n-"
+        ... )
+
+        >>> ss.summarize()
+        n-inflammatory-inflammatory               31
+        n-inflammatory-connective                 89
+        n-inflammatory-glandular_epithel           0
+        n-inflammatory-squamous_epithel            0
+        n-inflammatory-neoplastic                 86
+        n-connective-connective                  131
+        n-connective-glandular_epithel             0
+        n-connective-squamous_epithel              0
+        n-connective-neoplastic                  284
+        n-glandular_epithel-glandular_epithel      0
+        n-glandular_epithel-squamous_epithel       0
+        n-glandular_epithel-neoplastic             0
+        n-squamous_epithel-squamous_epithel        0
+        n-squamous_epithel-neoplastic              0
+        n-neoplastic-neoplastic                  236
+        dtype: int64
         """
         self.prefix = prefix
         self.classes = classes
-        self.spatial_context = spatial_context
-
-        if not isinstance(spatial_context, _SpatialContext):
-            raise ValueError(
-                "Input needs to be a class ingerited from the `_SpatialContext` object."
-                f" Got: {type(spatial_context)}."
-            )
+        self.spatial_weights = spatial_weights
+        self.gdf = gdf
 
     @staticmethod
     def get_link_counts(
@@ -131,25 +130,18 @@ class SpatialWeightSummary(Summary):
         """Return link proportions instead of counts."""
         return self.link_counts.div(self.link_counts.sum(axis=1), axis=0)
 
-    def summarize(
-        self,
-        key: str,
-        filter_pattern: Optional[str] = None,
-    ) -> pd.Series:
+    def summarize(self, filter_pattern: Optional[str] = None) -> pd.Series:
         """Summarize the cell networks.
 
         Parameters
         ----------
-            key : str
-                The network name to summarize. One of: "full_network", "border_network",
-                "interface_network", "roi_network".
             filter_pattern : str, optional
                 A string pattern. All off the values containing this pattern
                 in the result pd.Series are filtered out.
 
         Raises
         ------
-            ValueError: I illegal  key is given.
+            ValueError: If illegal key is given.
 
         Returns
         -------
@@ -157,27 +149,22 @@ class SpatialWeightSummary(Summary):
                 A summary vector containing summary features of the cell network
                 objects found in the `spatial_context`.
         """
-        allowed = ("full_network", "border_network", "interface_network", "roi_network")
-        if key not in allowed:
-            raise ValueError(f"Illegal key. Got: {key}. Allowed: {allowed}")
+        counts: pd.Series = self.get_link_counts(
+            self.gdf, self.spatial_weights, classes=self.classes
+        )
+        df = pd.DataFrame(counts, columns=["count"])
+        link_summary = df.loc[~(df == 0).all(axis=1)]
 
-        prefix = self.prefix if self.prefix is not None else ""
-        counts = []
-        for i, con in self.spatial_context.context.items():
-            datum = pd.Series({"label": i})
-            link_c = self.get_link_counts(
-                self.spatial_context.cell_gdf, con[key], self.classes
-            )
-            datum = pd.concat([datum, link_c.add_prefix(f"{prefix}")])
-            counts.append(datum)
-
-        df = pd.DataFrame(counts).set_index("label")
-        self.link_counts = df.loc[~(df == 0).all(axis=1)]
-
-        self.summary = self.link_counts.sum(axis=0)
         if filter_pattern is not None:
-            self.summary = self.summary.loc[
-                ~self.summary.index.str.contains(filter_pattern)
+            link_summary = link_summary.loc[
+                ~link_summary.index.str.contains(filter_pattern)
             ]
+
+        if self.prefix is not None:
+            link_summary = link_summary.set_index(
+                self.prefix + link_summary.index.astype(str)
+            )
+
+        self.summary = link_summary.squeeze()
 
         return self.summary
