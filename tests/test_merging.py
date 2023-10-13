f04d33416c0e259a3dc458e8c1f0ac8c7d38eadb
Okunator <oskari.lehtonen@helsinki.fi>
2023-10-13 14:41:49 +0300

    test: update merge tests

--- tests/test_merging.py
+++ tests/test_merging.py
@@ -3,9 +3,9 @@ from cellseg_gsontools.merging import AreaMerger, CellMerger
 
 def test_cell_merging(merge_data_cell):
     merger = CellMerger(merge_data_cell, tile_size=(1000, 1000))
-    merger.merge_dir(in_qupath_format="old", verbose=False)
+    merger.merge_dir(verbose=False)
 
 
 def test_area_merging(merge_data_area):
-    merger = AreaMerger(merge_data_area, tile_size=(1000, 1000))
-    merger.merge_dir(in_qupath_format="old", verbose=False)
+    merger = AreaMerger(merge_data_area)
+    merger.merge_dir(verbose=False)
