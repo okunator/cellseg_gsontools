d2a8f99a2da628fa2444aa840fc5ea40abd33dfc
Okunator <oskari.lehtonen@helsinki.fi>
2023-10-20 11:56:56 +0300

    feat: add `local_distances` function

--- cellseg_gsontools/character.py
+++ cellseg_gsontools/character.py
@@ -5,10 +5,10 @@ import numpy as np
 from libpysal.weights import W
 
 from cellseg_gsontools.apply import gdf_apply
-from cellseg_gsontools.neighbors import neighborhood, nhood_vals
+from cellseg_gsontools.neighbors import neighborhood, nhood_dists, nhood_vals
 from cellseg_gsontools.utils import set_uid
 
-__all__ = ["reduce", "local_character"]
+__all__ = ["reduce", "local_character", "local_distances"]
 
 
 def reduce(
@@ -138,7 +138,7 @@ def local_character(
 
     # Get the immediate node neighborhood
     data["nhood"] = gdf_apply(
-        data, neighborhood, col="uid", spatial_weights=spatial_weights, parallel=False
+        data, neighborhood, col=id_col, spatial_weights=spatial_weights, parallel=False
     )
 
     # get areas
@@ -175,6 +175,120 @@ def local_character(
 
         if rm_nhood_cols:
             data = data.drop(labels=[char_col], axis=1)
+
+    if rm_nhood_cols:
+        labs = ["nhood"]
+        if weight_by_area:
+            labs.append(area_col)
+        data = data.drop(labels=labs, axis=1)
+
+    return data
+
+
+def local_distances(
+    gdf: gpd.GeoDataFrame,
+    spatial_weights: W,
+    id_col: str = None,
+    reductions: Tuple[str, ...] = ("mean",),
+    weight_by_area: bool = False,
+    parallel: bool = False,
+    rm_nhood_cols: bool = True,
+    col_prefix: str = None,
+    create_copy: bool = True,
+) -> gpd.GeoDataFrame:
+    """Compute the local sum/mean/median of the neighborhood distances for each row.
+
+    NOTE: Option to weight the nhood values by their area before reductions.
+
+    Parameters
+    ----------
+        gdf : gpd.GeoDataFrame
+            The input GeoDataFrame.
+        spatial_weights : libysal.weights.W
+            Libpysal spatial weights object.
+        id_col : str, default=None
+            The unique id column in the gdf. If None, this uses `set_uid` to set it.
+        reductions : Tuple[str, ...], default=("sum", )
+            A list of reduction methods for the neighborhood. One of "sum", "mean",
+            "median".
+        weight_by_area : bool, default=False
+            Flag wheter to weight the neighborhood values by the area of the object.
+        parallel : bool, default=False
+            Flag whether to use parallel apply operations when computing the character
+        rm_nhood_cols : bool, default=True
+            Flag, whether to remove the extra neighborhood columns from the result gdf.
+        col_prefix : str, optional
+            Prefix for the new column names.
+        create_copy : bool, default=True
+            Flag whether to create a copy of the input gdf and return that.
+
+    Returns
+    -------
+        gpd.GeoDataFrame:
+            The input geodataframe with computed distances column added.
+
+    Examples
+    --------
+    Compute the mean of eccentricity values for each neighborhood
+    >>> from libpysal.weights import DistanceBand
+    >>> from cellseg_gsontools.character import local_character
+
+    >>> w_dist = DistanceBand.from_dataframe(gdf, threshold=55.0, alpha=-1.0)
+    >>> local_distances(
+    ...     gdf,
+    ...     spatial_weights=w_dist,
+    ...     reduction=["mean"],
+    ...     weight_by_area=True
+    ... )
+    """
+    allowed = ("mean", "median", "sum")
+    if not all(r in allowed for r in reductions):
+        raise ValueError(
+            f"Illegal reduction in `reductions`. Got: {reductions}. "
+            f"Allowed reductions: {allowed}."
+        )
+
+    if create_copy:
+        data = gdf.copy()
+    else:
+        data = gdf
+
+    # set uid
+    if id_col is None:
+        id_col = "uid"
+        data = set_uid(data)
+
+    # get the immediate node neighborhood
+    data["nhood"] = gdf_apply(
+        data, neighborhood, col=id_col, spatial_weights=spatial_weights
+    )
+
+    # get areas
+    area_col = None
+    if weight_by_area:
+        area_col = "dist_nhood_areas"
+        data[area_col] = gdf_apply(
+            data, nhood_vals, col="nhood", values=data.geometry.area, parallel=False
+        )
+
+    # get distances
+    data["nhood_dists"] = gdf_apply(
+        data,
+        nhood_dists,
+        col="nhood",
+        centroids=gdf.centroid,
+    )
+
+    # loop over the reduction methods
+    for r in reductions:
+        data[f"{col_prefix}nhood_dists_{r}"] = gdf_apply(
+            data,
+            reduce,
+            col="nhood_dists",
+            parallel=parallel,
+            how=r,
+            extra_col=area_col,
+        )
 
     if rm_nhood_cols:
         labs = ["nhood"]
--- tests/test_character.py
+++ tests/test_character.py
@@ -2,7 +2,7 @@ import pytest
 from libpysal.weights import DistanceBand
 
 from cellseg_gsontools.apply import gdf_apply
-from cellseg_gsontools.character import local_character
+from cellseg_gsontools.character import local_character, local_distances
 from cellseg_gsontools.geometry import eccentricity
 from cellseg_gsontools.utils import set_uid
 
@@ -22,6 +22,25 @@ def test_character(cell_gson, reduction, weight, parallel):
         w_dist,
         reductions=[reduction],
         val_col="eccentricity",
+        weight_by_area=weight,
+        parallel=parallel,
+        rm_nhood_cols=True,
+    )
+
+
+@pytest.mark.parametrize("reduction", ["sum", "mean", "median"])
+@pytest.mark.parametrize("weight", [True, False])
+@pytest.mark.parametrize("parallel", [True, False])
+def test_dists(cell_gson, reduction, weight, parallel):
+    gdf = cell_gson
+
+    data = set_uid(gdf)
+    w_dist = DistanceBand.from_dataframe(data, ids="uid", threshold=55.0, alpha=-1.0)
+
+    data = local_distances(
+        data,
+        w_dist,
+        reductions=[reduction],
         weight_by_area=weight,
         parallel=parallel,
         rm_nhood_cols=True,
